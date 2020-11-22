f5316bf814ebd93f5bc96ae153c57c9def51f84e
krisswiss <chrisswiatek@hotmail.com>
2020-11-22 13:49:39 +0000

    deployment - corrected typo in settings to allow static files to be used from AWS

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,7 +180,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'Ccustom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'Custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
